fa5e16f5aff255da76e4dc9d4aefcb227d8bcb3b
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-11-08 12:30:46 +0000

    update naming, set content type and return as json

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const reqRes = [
 
 const statusCodes = [
   200, // default (success)
-  404,
+  404, // not found
 ]
 
 const config = {
@@ -31,8 +31,11 @@ const config = {
       name: 'Access-Control-Allow-Origin',
       value: '*',
     },
+    {
+      name: 'Content-Type',
+      value: 'application/json',
+    },
   ],
-  statusCode: statusCodes[0],
 }
 
 const server = http.createServer()
@@ -43,9 +46,6 @@ server.on('request', (req, res) => {
     res.setHeader(header.name, header.value)
   })
 
-  // Set status code
-  res.statusCode = config.statusCode
-
   // Get key value pairs from the query string
   const parsedUrl = url.parse(req.url, true)
   const queryAsObject = parsedUrl.query
@@ -55,14 +55,19 @@ server.on('request', (req, res) => {
   const reqValue = queryAsObject[QS_KEY_1]
   console.log('reqValue: ', reqValue)
 
+  let resValue
+
   // Lookup return value for first query string value
-  const resValue = reqRes.find(
-    (rr) => rr.req.toLowerCase() === reqValue?.toLowerCase()
-  )?.res
+  resValue =
+    reqRes.find((rr) => rr.req.toLowerCase() === reqValue?.toLowerCase())
+      ?.res || null
+
   console.log('resValue: ', resValue)
 
+  res.statusCode = resValue ? statusCodes[0] : statusCodes[1]
+
   // Return request response
-  res.end(resValue)
+  res.end(JSON.stringify(resValue))
 })
 
 server.listen(PORT, () => {
