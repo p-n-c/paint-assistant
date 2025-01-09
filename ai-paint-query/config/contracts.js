4b664bc0ed367cfec8ab8d78d5a25971fbe2a4d1
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2025-01-09 12:35:11 +0100

    Removed old query system

--- ai-paint-query/config/contracts.js
+++
@@ -1,3 +0,0 @@
-export const simpleQuery = `Single parameter query for a Paint Database API. 
-Available API parameters: code, name, temperature, maxvoc
-Return only JSON object. Example: "paint for 35 degrees" -> {"temperature": 35}`
